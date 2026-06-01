15f3ad2a9324f96257420efe7434a9ca7a257fda
Ethan Berry <ethanberry670@gmail.com>
2026-06-01 14:28:29 +0100

    WIP: Add data models

---
+++ firstDoorFrontend/Models/JobNote.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace firstDoorFrontEnd.Models
+{
+    public class JobNote
+    {
+        public int Id { get; set; }
+        [MaxLength(500)] public string? Body { get; set; }
+        public DateTime DateSaved { get; set; } = DateTime.Now;
+        public int SavedJobId { get; set; }
+        public SavedJob SavedJob { get; set; } = new SavedJob();
+    }
+}
---
+++ firstDoorFrontend/Models/SavedJob.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace firstDoorFrontEnd.Models
+{
+    public class SavedJob
+    {
+        public int Id { get; set; }
+        [Required][MaxLength(100)] public string Title { get; set; } = string.Empty;
+        [Required][MaxLength(50)] public string EmployerName { get; set; } = string.Empty;
+        [Required][MaxLength(500)] public string Description { get; set; } = string.Empty;
+        [Required][MaxLength(50)] public string Location { get; set; } = string.Empty;
+        [Required][MaxLength(500)] public string Url { get; set; } = string.Empty;
+        public Status Status { get; set; } = 0;
+        public DateTime TimeSaved { get; set; } = DateTime.Now;
+        public List<JobNote> Notes { get; set; } = new();
+    }
+
+    public enum Status
+    {
+        To_Apply,
+        Applied,
+        Interviewing,
+        Rejected
+    }
+}
