ad86874791e5e8c8945c94bd0db4e7f6aaf069ba
Ethan Berry <ethanberry670@gmail.com>
2026-06-03 14:31:37 +0100

    WIP: Add dto model

---
+++ firstDoorFrontend/Models/PostNoteRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace firstDoorFrontend.Models
+{
+    public record PostNoteRequest
+    (
+        [Required][MaxLength(500)] string Body,
+        [Required] int SavedJobId
+    );
+}
