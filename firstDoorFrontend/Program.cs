a0d5bfe3cceff29d562937748d3a037e3c1ab189
Ethan Berry <ethanberry670@gmail.com>
2026-06-05 10:05:51 +0100

    Remove comments

--- firstDoorFrontend/Program.cs
+++ firstDoorFrontend/Program.cs
@@ -8,7 +8,6 @@ namespace firstDoorFrontend
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents()
                 .AddInteractiveWebAssemblyComponents();
@@ -17,7 +16,6 @@ namespace firstDoorFrontend
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseWebAssemblyDebugging();
@@ -25,7 +23,7 @@ namespace firstDoorFrontend
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                
                 app.UseHsts();
             }
 
